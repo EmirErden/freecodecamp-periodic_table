3f521b3624ad7456fd60b315f7a0bdce0975cf88
Emir Erdeb <emirerden123@gmail.com>
2024-10-27 21:15:15 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+elif [[ $1 =~ ^-?[0-9]+$ ]]
+
+
+
+
+fi
