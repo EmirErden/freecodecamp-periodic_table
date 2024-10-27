a8948a6e09b70510bdfc27043bedee7dc17b4f51
Emir Erdeb <emirerden123@gmail.com>
2024-10-27 21:15:55 +0000

    feat: Added integer part

--- element.sh
+++ element.sh
@@ -5,7 +5,21 @@ if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
 elif [[ $1 =~ ^-?[0-9]+$ ]]
-
+then
+  CHECK_DATABASE=$($PSQL "Select * From elements Where atomic_number=$1")
+  if [[ -z $CHECK_DATABASE ]]
+  then
+    echo I could not find that element in the database.
+  else
+    echo "$($PSQL "Select * From properties Where atomic_number = '$1'")" | while IFS="|" read ATOMIC_NUMBER ATOMIC_MASS MELTING BOILING TYPE_ID
+    do
+    TYPE=$($PSQL "Select type From types Where type_id=$TYPE_ID")
+    NAME=$($PSQL "Select name From elements Where atomic_number=$ATOMIC_NUMBER")
+    SYMBOL=$($PSQL "Select symbol From elements Where atomic_number=$ATOMIC_NUMBER")
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+    done
+  fi
+elif [[ ${#1} -eq 1 && $1 =~ ^[a-zA-Z]$ ]]
 
 
 
